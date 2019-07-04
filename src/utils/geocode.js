94dc8ee0a15b1fc59397cfa8e4caa1da4fc08b9a
Gianandrea Tognini <gianandrea.tognini@gmail.com>
2019-07-04 22:44:41 +0200

    Geocode and ForecastAPI Keys updated

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const geocode = (address, callback) => {
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiYW5kcmV3bWVhZDEiLCJhIjoiY2pvOG8ybW90MDFhazNxcnJ4OTYydzJlOSJ9.njY7HvaalLEVhEOIghPTlw&limit=1'
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1Ijoic2lsdmVzdGVyY2F0NzMiLCJhIjoiY2p4bGt3MTN4MDI3MzNwb2J4ejl1d3hvayJ9.bzXDuTJC7kw7T-Ufc3J6-A&limit=1'
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
