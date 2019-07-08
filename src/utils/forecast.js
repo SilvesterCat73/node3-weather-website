a94b606b0c377f209f07146496961e4d7f8e55de
Gianandrea Tognini <gianandrea.tognini@gmail.com>
2019-07-08 11:28:18 +0200

    Added daily Highest and Lowest temperature to the returned results

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,17 +1,34 @@
-const request = require('request')
+const request = require('request');
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/b20c83ff606c1b3c33355bdb64b804e7/' + latitude + ',' + longitude + '?units=si'
+    const url =
+        'https://api.darksky.net/forecast/b20c83ff606c1b3c33355bdb64b804e7/' +
+        latitude +
+        ',' +
+        longitude +
+        '?units=si';
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
-            callback('Unable to connect to weather service!', undefined)
+            callback('Unable to connect to weather service!', undefined);
         } else if (body.error) {
-            callback('Unable to find location', undefined)
+            callback('Unable to find location', undefined);
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(
+                undefined,
+                body.daily.data[0].summary +
+                    ' It is currently ' +
+                    body.currently.temperature +
+                    ' degrees out. The highest temperature for today will be around ' +
+                    body.daily.data[0].temperatureHigh +
+                    ' degrees and the lowest around ' +
+                    body.daily.data[0].temperatureLow +
+                    ' degrees.There is a ' +
+                    body.currently.precipProbability +
+                    '% chance of rain.'
+            );
         }
-    })
-}
+    });
+};
 
-module.exports = forecast
+module.exports = forecast;
